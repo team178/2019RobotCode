ee664f9d62b2e954b20f1ffd7984d64259f01a21
Darren Kwee <20kweeda@fpsct.org>
2019-02-07 17:48:34 -0500

    Converted all Edits in MiniBot Code to 2019 Robot Code

--- src/main/java/frc/robot/commands/AlignHatchPanel.java
+++ src/main/java/frc/robot/commands/AlignHatchPanel.java
@@ -9,36 +9,47 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.OI;
-import frc.robot.subsystems.Arduino;
 import frc.robot.Robot;
-import frc.robot.RobotMap.SubsystemIndex;
+import frc.robot.subsystems.Arduino;
+import frc.robot.subsystems.LinearActuator;
 
-public class SendMessage extends Command {
-
+public class AlignHatchPanel extends Command {
+  Arduino pixyArduino;
   OI oi;
-  Arduino arduino;
-  String t; 
+  LinearActuator linearactuator;
 
-  public SendMessage(String t) {
-  this.t = t;
+  public AlignHatchPanel() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
     oi = Robot.oi;
-    arduino = Robot.arduino;
+    pixyArduino = Robot.arduino;
+    linearactuator = Robot.linearactuator;
   }
-
-
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    arduino.sendMessage(SubsystemIndex.ALL, t);
+    double desiredavg = 159;
+    pixyArduino.checkForPixyValues();
+    int firstLocation = pixyArduino.firstLocation;
+    int secondLocation = pixyArduino.secondLocation;
+    double x1 = (double) firstLocation;
+    double x2 = (double) secondLocation; 
+    double avg = (x1 + x2)/2;
+    while(avg > (desiredavg  + 10) || avg < (desiredavg - 10)){
+      double diff = desiredavg-avg;
+      if (diff>desiredavg){
+        linearactuator.moveActuator(false);
+      } else {
+        linearactuator.moveActuator(true);
+      }
+    }
   }
-
-
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
@@ -46,19 +57,15 @@ public class SendMessage extends Command {
     return false;
   }
 
-
   // Called once after isFinished returns true
   @Override
   protected void end() {
   }
-
-
+  
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-
   }
-
 }
