ef31094a49516bcf542cd4fe8af46a387b234c42
FiveBillionHies <20fishelro@fpsct.org>
2019-02-14 19:45:17 -0500

    Made changes to HatchMechanism stuff. Also put LinearActuator functionality in HatchMechanism

--- src/main/java/frc/robot/commands/ExtendHatchMechanism.java
+++ src/main/java/frc/robot/commands/ExtendHatchMechanism.java
@@ -60,11 +60,13 @@ public class ExtendHatchMechanism extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    hatchmechanism.setExtender("reverse");
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    hatchmechanism.setExtender("reverse");
   }
 }
