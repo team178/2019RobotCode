ab31e9d4e532006a84edc1a0325517ccfc5d7d11
178Laptop <programming@farmingtonrobotics.org>
2019-02-18 18:26:48 -0500

    Switched isfinished in alignhatchpanel from and to or

--- src/main/java/frc/robot/commands/AlignHatchPanel.java
+++ src/main/java/frc/robot/commands/AlignHatchPanel.java
@@ -69,7 +69,7 @@ public class AlignHatchPanel extends Command
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return (Math.abs(diff) < TOLERANCE && !triggerPressed);
+    return (Math.abs(diff) < TOLERANCE || !triggerPressed);
   }
 
   // Called once after isFinished returns true
