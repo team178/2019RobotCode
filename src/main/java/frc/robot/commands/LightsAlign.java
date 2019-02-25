5b0a8211310c9b1fddcbb5a2ecc25b29aec45ddb
Yash Sabarad <30813396+YashSabarad@users.noreply.github.com>
2019-02-25 18:46:05 -0500

    changed message

--- src/main/java/frc/robot/commands/LightsAlign.java
+++ src/main/java/frc/robot/commands/LightsAlign.java
@@ -31,7 +31,7 @@ public class LightsAlign extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    sent = lightsArduino.sendMessage("g");
+    sent = lightsArduino.sendMessage("a");
   }
 
   // Make this return true when this Command no longer needs to run execute()
