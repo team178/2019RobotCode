e478425cfbea43206f60d716cb2f3721cf28c917
FiveBillionHies <34493033+FiveBillionHies@users.noreply.github.com>
2019-03-14 18:44:16 -0400

    finished execute; need to update isFinished() conditions

--- src/main/java/frc/robot/commands/AutoClimb.java
+++ src/main/java/frc/robot/commands/AutoClimb.java
@@ -14,6 +14,11 @@ public class AutoClimb extends Command {
   OI oi;
   Climber climber;
   
+  private final double POWER = 1.00;
+  private double overidePowerFront;
+  private double overridePowerBack;
+  private boolean finished;
+  
   public AutoClimb() {
     requires(Robot.climber);
   }
@@ -28,22 +33,48 @@ public class AutoClimb extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    overridePowerFront = oi.getLeftStickYAux();
+    overridePowerBack = -oi.getRightStickYAux();
+    
+    //Front climber
+    if (Math.abs(overridePowerFront) < 0.1){
+      climber.moveFrontMotors(0);
+      finished = true;
+    } else if (climber.isFrontClimberAtTop()) {
+      climber.moveFrontMotors(0);
+      finished = true;
+    } else {
+      climber.moveFrontMotors(POWER);
+    }
+
+    //Back climb
+    if (Math.abs(overridePowerBack) < 0.2){
+      climber.moveBackMotors(0);
+      finished = true;
+    } else if (climber.isBackClimberAtTop()) {
+      climber.moveBackMotors(0);
+      finished = true;
+    } else {
+      climber.moveBackMotors(-POWER);
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return isFrontClimberAtTop() && isBackClimberAtTop();
+    return finished;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    climber.moveBackMotors(0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    climber.moveBackMotors(0);
   }
 }
