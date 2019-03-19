488224a0879337e32c00a6d3521c95510ae72efa
FiveBillionHies <34493033+FiveBillionHies@users.noreply.github.com>
2019-03-18 22:35:39 -0400

    CalibrateHatch command prints out approximate time to center of hatch mech

--- src/main/java/frc/robot/commands/CalibrateHatchSlide.java
+++ src/main/java/frc/robot/commands/CalibrateHatchSlide.java
@@ -7,38 +7,62 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
+import frc.robot.OI;
+import frc.robot.Robot;
+import frc.robot.subsystems.HatchMechanism;
+
 public class CalibrateHatchSlide extends Command {
+  
+  private static Timer timer = new Timer();
+  OI oi;
+  HatchMechanism hatchMechanism;
+  
+  private double calibratedTimeToCenter;
+  
   public CalibrateHatchSlide() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+    requires(Robot.hatchMechanism);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    oi = Robot.oi;
+    hatchMechanism = Robot.hatchMechanism;
+    calibratedTimeToCenter = 0;
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    hatchMechanism.leadScrewToLeft();
+    timer.reset();
+    
+    while (!hatchMechanism.hasReachedLeftBound()) {
+      hatchMechanism.moveLeadScrew(true, 1);
+    }
+    calibratedTimeToCenter = timer.get() / 2;
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    hatchMechanism.moveLeadScrew(true, 0);
+    System.out.println("Approximate time to center mechanism: " + calibratedTimeToCenter + " seconds");
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    hatchMechanism.moveLeadScrew(true, 0);
   }
 }
